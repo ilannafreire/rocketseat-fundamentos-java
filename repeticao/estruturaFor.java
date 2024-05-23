6433f189bc7b7e2ae77e5eb923f3787e579aba5f
Ilanna Freire <ilannafreiret@hotmail.com>
2024-05-23 18:31:52 -0300

    feat: adicionando estrutura for

---
+++ repeticao/estruturaFor.java
@@ -0,0 +1,11 @@
+package repeticao;
+
+public class estruturaFor {
+    public static void main(String[] args) {
+        System.out.println("Iniciando o For");
+        for (int i = 0; i < 4; i++){
+            System.out.println("O valor do i é: " + i);
+        }
+        System.out.println("Finalizando o For");
+    }
+}
