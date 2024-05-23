51cc382e69d861fa095416923234a0e73d9299c4
Ilanna Freire <ilannafreiret@hotmail.com>
2024-05-23 16:34:07 -0300

    feat: adicionando tipos de dados java

---
+++ dados/dadosJava.java
@@ -0,0 +1,18 @@
+package dados;
+
+public class dadosJava {
+    public static void main(String[] args) {
+        /*
+        * Valores(int, double, float, long)
+        * Texto (String)
+        * Booleanos(boolean- verdadeiro, falso)
+         */
+        int dadosDoTipoInt = 10;
+        double dadosDoTipoDouble = 1.14;
+        float dadosDoTipoFloat = 3.14f;
+        long dadosDoTipoLong = 89347234423949L;
+        String dadosDoTipoString = "Colocar o seu texto";
+        boolean dadosDoTipoBoolean = false;
+
+    }
+}
