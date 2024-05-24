c0473ca2ecc07cc2e0f82231a32533fc7bebdc39
Ilanna Freire <ilannafreiret@hotmail.com>
2024-05-24 16:45:48 -0300

    feat: adicionando POO conta bancaria

---
+++ conta/ContaBancaria.java
@@ -0,0 +1,27 @@
+package conta;
+
+public class ContaBancaria {
+
+    Sring numero;
+    String titular;
+    double saldo;
+
+    // depositar
+    void depositar(double valor){
+        if(valor > 0){
+            saldo = saldo + valor;
+            System.out.println("Deposito de R$ " + valor + ". Saldo atual R$ " + saldo);
+        }else {
+            System.out.println("O valor de depósito é inválido");
+        }
+    }
+    //sacar
+
+    void sacar(double valor){
+         if (valor > 0 && valor <= saldo){
+             saldo = saldo - valor;
+             System.out.println("Saque de R$ " + valor);
+
+         }
+    }
+}
