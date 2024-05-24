35b8b61f4af73de37af72e0c65294fba1ce6c7ef
Ilanna Freire <ilannafreiret@hotmail.com>
2024-05-24 18:59:00 -0300

    feat: add getters e setters em conta bancaria

--- conta/ContaBancaria.java
+++ conta/ContaBancaria.java
@@ -2,9 +2,35 @@ package conta;
 
 public class ContaBancaria {
 
-    String numero;
-    String titular;
-    double saldo;
+    private String numero;
+    private String titular;
+    private double saldo;
+
+     // Getters e Setters
+
+    public void setNumero(String numero){
+        this.numero = numero;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
 
     // depositar
     void depositar(double valor){
@@ -25,3 +51,10 @@ public class ContaBancaria {
          }
     }
 }
+
+
+/* Getters e Setters são métodos que criamos para manipulaçao de busca
+de informaçao ou para adicionar.
+*
+* Getters para recuperar valores
+ */
--- conta/TesteContaBancaria.java
+++ conta/TesteContaBancaria.java
@@ -6,16 +6,21 @@ public class TesteContaBancaria {
     public static void main(String[] args) {
 
         ContaBancaria contaBancaria1 = new ContaBancaria();
-        contaBancaria1.numero = "1234";
-        contaBancaria1.titular = "Nívia";
-        contaBancaria1.saldo = 100;
+        contaBancaria1.setNumero("1234");
+
+        contaBancaria1.setTitular("Nívia");
+        contaBancaria1.setSaldo(100);
 
         contaBancaria1.depositar(50);
         contaBancaria1.sacar(100);
     }
 }
 
+
+
 /*
-* Encapsulamento:
+* Encapsulamento: proteção de dados. usuário pode buscar o atributo e/ou
+* pode adicionar informação nesse novo atributo.
+*
 *
  */
